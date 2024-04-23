df5dde755e8ff78c6e153cecac8659f1d1d3193a
Zofia Stawarska <z.stawarska@gmail.com>
2024-04-23 22:29:03 +0200

    useParams & useSearchParams

--- world-wise/src/components/Map.jsx
+++ world-wise/src/components/Map.jsx
@@ -1,10 +1,18 @@
+import { useSearchParams } from 'react-router-dom'
 import styles from './Map.module.css'
 
 function Map() {
+  const [searchParams, setSearchParams] = useSearchParams()
+  const lat = searchParams.get('lat')
+  const lng = searchParams.get('lng')
+
   return (
     <div className={styles.mapContainer}>
-      
-    </div>
+      <h1>{lat} {lng}</h1>
+      <button onClick={() => setSearchParams({lat: 22, lng: 4000})}>
+        Change location
+      </button>
+    </div> 
   )
 }
 
