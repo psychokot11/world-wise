c0391e46c756e24ad5992085a5711267b639aae1
Zofia Stawarska <z.stawarska@gmail.com>
2024-07-23 19:32:52 +0200

    add fake login

--- world-wise/src/components/Form.jsx
+++ world-wise/src/components/Form.jsx
@@ -1,6 +1,6 @@
 import { useUrlPosition } from "../hooks/useUrlPosition"
 import { useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom"
 import DatePicker from "react-datepicker"
 import "react-datepicker/dist/react-datepicker.css"
 import { useCities } from "../contexts/CitiesContext"
--- world-wise/src/components/User.jsx
+++ world-wise/src/components/User.jsx
@@ -1,16 +1,15 @@
-import styles from "./User.module.css";
-
-const FAKE_USER = {
-  name: "Jack",
-  email: "jack@example.com",
-  password: "qwerty",
-  avatar: "https://i.pravatar.cc/100?u=zz",
-};
+import { useAuth } from "../contexts/FakeAuthContext"
+import { useNavigate } from "react-router-dom"
+import styles from "./User.module.css"
 
 function User() {
-  const user = FAKE_USER;
+  const { user, logout } = useAuth()
+  const navigate = useNavigate()
 
-  function handleClick() {}
+  function handleClick() {
+    logout()
+    navigate("/")
+  }
 
   return (
     <div className={styles.user}>
@@ -21,7 +20,7 @@ function User() {
   );
 }
 
-export default User;
+export default User
 
 /*
 CHALLENGE
