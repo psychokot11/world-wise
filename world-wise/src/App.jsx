26ddfd6d842ed236715efca612f9003f7f6cb0ea
Zofia Stawarska <z.stawarska@gmail.com>
2024-07-23 19:55:34 +0200

    add protecting route component

--- world-wise/src/App.jsx
+++ world-wise/src/App.jsx
@@ -6,6 +6,7 @@ import Pricing from './pages/Pricing'
 import Login from './pages/Login'
 import PageNotFound from './pages/PageNotFound'
 import AppLayout from './pages/AppLayout'
+import ProtectedRoute from './pages/ProtectedRoute'
 import CityList from './components/CityList'
 import CountryList from './components/CountryList'
 import City from './components/City'
@@ -23,7 +24,14 @@ function App() {
             <Route path="product" element={<Product />} />
             <Route path="pricing" element={<Pricing />} />
             <Route path="login" element={<Login />} />
-            <Route path="app" element={<AppLayout />}>
+            <Route 
+              path="app" 
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
               <Route index element={<Navigate replace to="cities" />} />
               <Route path="cities" element={<CityList />} />
               <Route path="cities/:id" element={<City />} />
