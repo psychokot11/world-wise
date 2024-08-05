c368b041a78ca5d395f34194d1f6d6d55597ed50
Zofia Stawarska <z.stawarska@gmail.com>
2024-08-05 21:17:55 +0200

    useCallback for optimization

--- world-wise/src/contexts/CitiesContext.jsx
+++ world-wise/src/contexts/CitiesContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useContext, useReducer } from 'react';
+import { createContext, useEffect, useContext, useReducer, useCallback } from 'react';
 
 const BASE_URL = 'http://localhost:5000'
 
@@ -59,19 +59,20 @@ function CitiesProvider({ children }) {
       fetchCities()
     }, [])
 
-    async function getCity(id) {
-      if(id === currentCity.id) return
+    const getCity = useCallback(
+      async function getCity(id) {
+        if(id === currentCity.id) return
 
-      dispatch({ type: 'loading' })
-      try {
-        const res = await fetch(`${BASE_URL}/cities/${id}`)
-        const data = await res.json()
-        dispatch({ type: 'city/loaded', payload: data })
-      }
-      catch {
-        dispatch({ type: 'rejected', payload: 'Error getting the city' })
-      }
-    }
+        dispatch({ type: 'loading' })
+        try {
+          const res = await fetch(`${BASE_URL}/cities/${id}`)
+          const data = await res.json()
+          dispatch({ type: 'city/loaded', payload: data })
+        }
+        catch {
+          dispatch({ type: 'rejected', payload: 'Error getting the city' })
+        }
+      }, [currentCity.id])
 
     async function createCity(newCity) {
       dispatch({ type: 'loading' })
